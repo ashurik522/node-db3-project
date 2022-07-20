0c84d2260066632b0828b376f3abf355d632a4b4
ashurik522 <ashurik522@gmail.com>
2022-07-20 17:27:44 -0400

    validate scheme middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -12,6 +12,7 @@ const checkSchemeId = async (req, res, next) => {
   let scheme = await db('schemes').where('scheme_id', req.params.scheme_id).first()
   if(scheme == null){
     res.status(404).json({ message: `scheme with scheme_id ${req.params.scheme_id} not found`})
+    return;
   }
   next()
 }
@@ -25,6 +26,10 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
+  if(typeof req.body.scheme_name !== 'string' || req.body.scheme_name.trim() === ""){
+    res.status(400).json({ message: 'invalid scheme_name'})
+    return;
+  }
   next()
 }
 
